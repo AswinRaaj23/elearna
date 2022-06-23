738f091d776b63c9dc297d0be28e9b1bb3e3cb99
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-23 17:50:38 +0530

    Create viewsets and routers and add additional actions to viewsets

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,6 +7,10 @@ from rest_framework.response import Response
 from ..models import Course
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+from rest_framework.decorators import action
+
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -16,11 +20,17 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
