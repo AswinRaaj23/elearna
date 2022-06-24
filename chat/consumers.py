b01baaf95fb615eac8f07edced4f5d83f7266a64
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-24 14:09:23 +0530

    Setup channel layer with redis and update the consumer to broadcast messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,18 +1,40 @@
-from email import message
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.id
+        
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+                      self.room_group_name,
+                      self.channel_name
+        )
         self.accept()   #accept connection
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+                      self.room_group_name,
+                      self.channel_name
+        )
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({'message': message}))
-
-
         
+        #send message to the room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    # receive message from the room group
+    def chat_message(self, event):
+    # Send message to the WebSocket
+        self.send(text_data=json.dumps(event)) 
